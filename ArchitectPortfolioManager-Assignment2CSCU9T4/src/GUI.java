ee656c1c41dac3cb0e0eff8719bc28335904c5f2
boostaboosh <98960726+lorenzo-2022@users.noreply.github.com>
2022-04-25 22:22:49 +0100

    Add action listeners to combobox and buttons

--- ArchitectPortfolioManager-Assignment2CSCU9T4/src/GUI.java
+++ ArchitectPortfolioManager-Assignment2CSCU9T4/src/GUI.java
@@ -1,10 +1,35 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 
 public class GUI {
 
+    public GUI() {
+        projectTypeJComboBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        editProjectButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        addProjectButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                
+            }
+        });
+    }
+
     public static void main(String[] args) {
         ProjectManager PM = new ProjectManager();
-        JFrame frame = new JFrame("GUI");
+        JFrame frame = new JFrame("Architect Portfolio");
         frame.setContentPane(new GUI().panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
