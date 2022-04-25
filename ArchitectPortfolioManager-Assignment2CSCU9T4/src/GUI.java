b863553a99a9fcf1dcd631261ca75c7d76e1604e
boostaboosh <98960726+lorenzo-2022@users.noreply.github.com>
2022-04-25 21:56:39 +0100

    Adds gui components to add / edit jpanel

--- ArchitectPortfolioManager-Assignment2CSCU9T4/src/GUI.java
+++ ArchitectPortfolioManager-Assignment2CSCU9T4/src/GUI.java
@@ -1,4 +1,63 @@
+import javax.swing.*;
+
 public class GUI {
+    private JPanel panel1;
+    private JButton addProjectButton;
+    private JTabbedPane tabbedPane1;
+    private JComboBox projectTypeJComboBox;
+    private JLabel classificationLabel;
+    private JTextField classificationTextField1;
+    private JButton editProjectButton;
+    private JTextArea textArea1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField5;
+    private JLabel projectTypeLabel;
+    private JLabel budgetLabel;
+    private JLabel managerLabel;
+    private JLabel locationLabel;
+    private JLabel newNotRenovationLabel;
+    private JLabel customerIDLabel;
+    private JLabel estimatedDurationLabel;
+    private JLabel estimatedStartLabel;
+    private JLabel actualStartLabel;
+    private JLabel actualEndLabel;
+    private JLabel accumulatedCostLabel;
+    private JPanel searchJPanel;
+    private JPanel addOrEditJPanel;
+    private JCheckBox checkBox1;
+    private JSlider slider1;
+    private JSpinner spinner1;
+    private JSpinner spinner2;
+    private JSpinner spinner3;
+    private JSpinner spinner4;
+    private JSpinner spinner5;
+    private JSpinner spinner6;
+    private JSpinner spinner7;
+    private JSpinner spinner8;
+    private JSpinner spinner9;
+    private JSpinner spinner10;
+    private JSpinner spinner11;
+    private JTextField textField1;
+    private JSlider slider2;
+    private JTextField textField4;
+    private JSpinner spinner12;
+    private JSpinner spinner13;
+    private JSpinner spinner14;
+    private JSpinner spinner15;
+    private JCheckBox checkBox2;
+    private JTextField textField6;
+    private JSpinner spinner16;
+    private JSpinner spinner17;
+    private JTextField textField7;
+    private JCheckBox checkBox3;
+    private JTextField textField8;
+    private JTextField textField9;
+    private JCheckBox checkBox4;
+    private JSpinner spinner18;
+    private JSpinner spinner19;
+
+
     public static void main(String[] args){
         ProjectManager PM = new ProjectManager();
     }
