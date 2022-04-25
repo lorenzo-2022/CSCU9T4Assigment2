ddcdf6a50f91dc0fd6a399d691b83b1da615b87f
boostaboosh <98960726+lorenzo-2022@users.noreply.github.com>
2022-04-25 19:09:59 +0100

    Read the CSV file into memory and construct the appropriate Java objects from each row

--- ArchitectPortfolioManager-Assignment2CSCU9T4/src/ProjectManager.java
+++ ArchitectPortfolioManager-Assignment2CSCU9T4/src/ProjectManager.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class ProjectManager {
@@ -15,8 +16,8 @@ public class ProjectManager {
 
     /**object constructor*/
     public ProjectManager(){
+        projects = new ArrayList<Project>(); //must initialise array list before the read() method
         readCSV();
-        projects = new ArrayList<Project>();
     }
 
     /**object methods*/
@@ -54,58 +55,93 @@ public class ProjectManager {
                 int budget = Integer.parseInt(budgetString);
                 String projectManager = projectInformation[11];
                 String location = projectInformation[12];
-                String newNotRenovation = projectInformation[13];
+                String newNotRenovationString = projectInformation[13];
+                boolean newNotRenovation = Boolean.parseBoolean(newNotRenovationString);
                 String customerID = projectInformation[14];
-                String estimatedDuration = projectInformation[15];
-                String estimatedStart = projectInformation[16];
-                String actualStart = projectInformation[17];
-                String actualEnd = projectInformation[18];
-                String accumulatedCost = projectInformation[19];
+                String estimatedDurationString = projectInformation[15];
+                int estimatedDuration = Integer.parseInt(estimatedDurationString);
+                String estimatedStartString = projectInformation[16];//convert to Calendar object
+                Calendar estimatedStart = stringToCalendarDate(estimatedStartString);
+                String actualStartString = projectInformation[17];//convert to Calendar object
+                Calendar actualStart = stringToCalendarDate(actualStartString);
+                String actualEndString = projectInformation[18];//convert to Calendar object
+                Calendar actualEnd = stringToCalendarDate(actualEndString);
+                String accumulatedCostString = projectInformation[19];
+                int accumulatedCost = Integer.parseInt(accumulatedCostString);
 
                 //type is for house and bridge projects
                 String type = projectInformation[1];
-                String floodingRisk = projectInformation[6];
+                String floodingRiskString = projectInformation[6];
+                int floodingRisk = stringToInt(floodingRiskString);
                 String buildingMaterial = projectInformation[8];
 
-                //size is for house and land projects but with different units of measurement
-                String size = projectInformation[2];
+                //size is for house and land projects but with different units of measurement, square metres for house, and acres for land
+                String sizeString = projectInformation[2];
+                int size = stringToInt(sizeString);
 
                 //these fields are for house projects only
-                String bedrooms = projectInformation[3];
-                String landSize = projectInformation[4];
-                String bathrooms = projectInformation[5];
-                String garage = projectInformation[7];
+                String bedroomsString = projectInformation[3];
+                int bedrooms =0;
+                if (!(bedroomsString.equals(""))) {
+                    bedrooms = (int) Float.parseFloat(bedroomsString);
+                }
+                String landSizeString = projectInformation[4];
+                float landSize = stringToFloat(landSizeString);
+                String bathroomsString = projectInformation[5];
+                int bathrooms = 0;
+                if (!(bathroomsString.equals(""))){
+                    bathrooms = (int) Float.parseFloat(bathroomsString);
+                }
+                String garageString = projectInformation[7];
+                boolean garage = Boolean.parseBoolean(garageString);
                 String roof = projectInformation[9];
 
                 //width and overlay is for bridge and tunnel
-                String width = projectInformation[21];
+                String widthString = projectInformation[21];
+                int width = stringToInt(widthString);
                 String overlay = projectInformation[22];
 
                 //excavating is for tunnel
                 String excavating = projectInformation[20];
-                String safetyTunnel = projectInformation[23];
-                String length = projectInformation[26];
-                String groundStructuralStability = projectInformation[28];
+                String safetyTunnelString = projectInformation[23];
+                boolean safetyTunnel = Boolean.parseBoolean(safetyTunnelString);
+                String lengthString = projectInformation[26];
+                int length = stringToInt(lengthString);
+                String groundStructuralStabilityString = projectInformation[28];
+                int groundStructuralStability = stringToInt(groundStructuralStabilityString);
 
                 //span is for bridge
-                String span = projectInformation[24];
+                String spanString = projectInformation[24];
+                int span = stringToInt(spanString);
 
                 //current composition is for land
                 String currentComposition = projectInformation[25];
-                String natureReserve = projectInformation[27];
+                String natureReserveString = projectInformation[27];
+                boolean natureReserve = Boolean.parseBoolean(natureReserveString);
 
                 //constructing the object and adding it to the projects array list
                 if (projectType.equals("House")){
                     //create and add house object to Project Manager array list
-                    Project project = new House(, , , , , , , , , , , , , , , , , , , , );
+                    Project project = new House(projectType, typeClassification, budget, projectManager, location, newNotRenovation,
+                            customerID, estimatedDuration, estimatedStart, actualStart, actualEnd, accumulatedCost,
+                            type, floodingRisk, buildingMaterial, size, bedrooms, landSize, bathrooms, garage, roof);
+                    addProject(project);
                 } else if (projectType.equals("Bridge")){
                     //create and add bridge object to Project Manager array list
+                    Project project = new Bridge(projectType, typeClassification, budget, projectManager, location, newNotRenovation, customerID, estimatedDuration, estimatedStart, actualStart, actualEnd, accumulatedCost,
+                            type, floodingRisk, buildingMaterial, width, overlay, span);
+                    addProject(project);
                 } else if (projectType.equals("Tunnel")){
                     //create and add tunnel object to Project Manager array list
+                    Project project = new Tunnel(projectType, typeClassification, budget, projectManager, location, newNotRenovation, customerID, estimatedDuration, estimatedStart, actualStart, actualEnd, accumulatedCost,
+                            width, overlay, excavating, safetyTunnel, length, groundStructuralStability);
+                    addProject(project);
                 } else if (projectType.equals("Land")){
                     //create and add land object to Project Manager arrayList
+                    Project project = new Land(projectType, typeClassification, budget, projectManager, location, newNotRenovation, customerID, estimatedDuration, estimatedStart, actualStart, actualEnd, accumulatedCost,
+                            size, currentComposition, natureReserve);
+                    addProject(project);
                 }
-
             }
 
             inputStreamReader.close();                               // File finished reading, so close file
@@ -113,6 +149,41 @@ public class ProjectManager {
         catch (IOException ex) {
             System.out.println("File reading error");    // File handling error: read no further
         }
+    }
+
+    private int stringToInt(String string) {
+        if (string.equals("")){
+            return 0;
+        }
+        else {
+            String numberOnlyString = string.replaceAll("[^0-9]", ""); //replacing all the parts of the sizeString that aren't numbers with nothing, i.e. removing those parts!
+            int numberOnly = Integer.parseInt(numberOnlyString);
+            return numberOnly;
+        }
+    }
+
+    private float stringToFloat(String string) {
+        if (string.equals("")){
+            return 0;
+        } else{
+            String numberOnlyString = string.replaceAll("[^\\d.]", ""); //replacing all the parts of the sizeString that aren't numbers or a dot (decimal dot) with nothing, i.e. removing those parts!
+            float floatOnly = Float.parseFloat(numberOnlyString);
+            return floatOnly;
+        }
+    }
+
+    //converts a string containing a date in format d/m/y to a Calendar object
+    private Calendar stringToCalendarDate(String dmy){
+        if (dmy.equals("")){
+            return null;
+        }
+        String[] calArray = dmy.split("-"); //forward slashes / are read as dashed -
+        int day = Integer.parseInt(calArray[0]);
+        int month = Integer.parseInt(calArray[1]);
+        int year = Integer.parseInt(calArray[2]);
+        Calendar calendar = (Calendar.getInstance());
+        calendar.set(year, month, day);
+        return calendar;
     }
 
     private void addProject(Project project) {
